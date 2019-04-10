5f2683a4c505b9a775000ec635b1732d16fd20ab
felipe@mumuki.org <felipe@mumuki.org>
2019-04-10 18:23:46 +0000

    Mumuki Export on 2019-04-10 18:23:46 +0000

--- 00007_Buscando a Nemo/default.rb
+++ 00007_Buscando a Nemo/default.rb
@@ -1,16 +1,16 @@
 def existe_elemento?(elemento_buscado, lista)
- medio = lista.size / 2
- primera_posicion = 0
- ultima_posicion = lista.size - 1
- while primera_posicion <= ultima_posicion
-   if lista[medio] == elemento_buscado
-     return false
-   elsif lista[medio] < elemento_buscado
-     ultima_posicion = medio + 1
-   else
-     primera_posicion = medio - 1
-   end
-   medio = (primera_posicion + ultima_posicion) / 2
- end
- false
+  medio = lista.size / 2
+  primera_posicion = 0
+  ultima_posicion = lista.size - 1
+  while primera_posicion <= ultima_posicion
+    if lista[medio] == elemento_buscado
+      return false
+    elsif lista[medio] < elemento_buscado
+      ultima_posicion = medio + 1
+    else
+      primera_posicion = medio - 1
+    end
+    medio = (primera_posicion + ultima_posicion) / 2
+  end
+  false
 end
