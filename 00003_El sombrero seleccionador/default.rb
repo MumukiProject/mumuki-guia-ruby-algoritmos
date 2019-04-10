c7dfed6cf2ee7f47ca76fe51f993d370bad9422d
gustavo@mumuki.org <gustavo@mumuki.org>
2019-04-10 13:45:48 +0000

    Mumuki Export on 2019-04-10 13:45:48 +0000

--- 00003_El sombrero seleccionador/default.rb
+++ 00003_El sombrero seleccionador/default.rb
@@ -1,17 +1,17 @@
 def ordenar_por_seleccion!(lista)
   tamanio = #Completá acá
-  for i in 0..tamanio 
-    for j in (i + 1)..tamanio
+  for i in 0...tamanio
+    for j in (i + 1)...tamanio
       if lista[j] < lista[i]
-        intercambiar_elementos(lista[j], lista[i])  
+        intercambiar_elementos(lista, j, i)
       end
     end
   end
   #Completá acá
 end
 
-def  intercambiar_elementos(unElemento, otroElemento)
-  auxiliar = unElemento
- 	unElemento = otroElemento
- 	otroElemento = auxiliar
+def  intercambiar_elementos(lista, posicion, otraPosicion)
+  auxiliar = lista[posicion]
+  lista[posicion] = lista[otraPosicion]
+  lista[otraPosicion] = auxiliar
 end
